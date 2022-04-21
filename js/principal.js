95a114249b66da418357c3a20d291ff2b3d584ad
JulyAnAr88 <july.an.ar88@gmail.com>
2022-04-21 17:45:25 -0300

    21/04 17:45

---
+++ js/principal.js
@@ -0,0 +1,10 @@
+var botonEncriptar = document.querySelector("#encriptador");
+
+botonAdicionar.addEventListener("click",function(event){
+    event.preventDefault();
+
+})
+
+function encriptar(textoAEncriptar){
+
+}
